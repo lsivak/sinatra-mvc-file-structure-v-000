0c1de29f603d74af7fa1f270208e5fd3036e4c2c
Lara Sanders <larasanders@gmail.com>
2017-12-13 18:50:20 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,7 +1,14 @@
 class Dog < Application::Base
   attr_accessor :name, :breed, :age
+  @@all = []
 
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
+  end
+
+  def save
+    Dog.save
+  end
+end
